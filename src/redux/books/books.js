31a22c9c90ac7f3e42d15fa6f7dbfd27659a09e9
Kelvin <ngotho.kelvin@yahoo.com>
2023-02-09 00:15:03 +0300

    Change default state in books reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,23 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    title: 'The Hunger Game',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
+  {
+    id: 3,
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+  },
+];
 
 export const addbook = (book) => ({
   type: ADD_BOOK,
